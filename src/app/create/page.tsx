6692fb93773eba54017e69b83e3ac5a37182c56a
Shashank <shashanke7y@gmail.com>
2023-08-18 12:28:15 -0400

    users can now create banquets

---
+++ src/app/create/page.tsx
@@ -0,0 +1,112 @@
+"use client";
+import HeaderComponent from "@/components/HeaderComponet";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, db } from "../../../firebase";
+import { doc, setDoc } from "firebase/firestore";
+import { title } from "process";
+
+const CreateBanquet = () => {
+  const [user, loading] = useAuthState(auth);
+  const [banquetTitle, setBanquetTitle] = useState("");
+  const [banquetDescription, setBanquetDescription] = useState("");
+  const [hashtags, setHashtags] = useState("");
+  const [hashtagsArray, setHashtagsArray] = useState([]);
+  const [banquetPasscode, setBanquetPasscode] = useState("");
+
+  const [errorMessage, setErrorMessage] = useState("");
+  const router = useRouter();
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  if (!user) {
+    router.push("/discover");
+  }
+
+  const handleInputChange = (e: any) => {
+    const inputValue = e.target.value;
+    inputValue.replace(/#/g, "");
+    setHashtags(inputValue);
+
+    const arrayFromInput = inputValue.split(" ");
+
+    setHashtagsArray(arrayFromInput);
+  };
+
+  const createBanquet = async () => {
+    await setDoc(doc(db, "Banquet", banquetTitle), {
+      title: banquetTitle,
+      description: banquetDescription,
+      hashtags: hashtagsArray,
+      passcode: banquetPasscode,
+    });
+    router.push("/discover");
+  };
+
+  return (
+    <div className="max-w-7xl flex flex-col">
+      <HeaderComponent profilePicture={user?.photoURL || ""} />
+      <div className="w-full ml-3">
+        <div>
+          <h1 className="text-xl font-semibold">Create your Banquet 🥂</h1>
+        </div>
+        <div className="mt-5 w-11/12">
+          <h1 className="mb-2 text-base font-bold">Name of your Banquet</h1>
+          <p className="font-semibold text-xs text-gray-500">
+            Enter a captivating name for your exclusive banquet.{" "}
+          </p>
+        </div>
+        <input
+          onChange={(e) => setBanquetTitle(e.target.value)}
+          value={banquetTitle}
+          className="w-11/12 mt-2 text-black border-2 px-3 text-sm rounded-lg border-black h-10"
+        />
+        <div className="mt-5 w-11/12">
+          <h1 className="mb-2 text-base font-bold">Add a description</h1>
+          <p className="font-semibold text-xs text-gray-500">
+            Write a compelling description that paints a vivid picture of what
+            your banquet is all about. Let your guests know what to expect and
+            why your event is a must-attend.
+          </p>
+        </div>
+        <textarea
+          onChange={(e) => setBanquetDescription(e.target.value)}
+          value={banquetDescription}
+          className="w-11/12 mt-2 pt-2 text-black border-2 px-3 text-sm rounded-lg border-black h-44"
+        />
+        <div className="mt-5 w-11/12">
+          <h1 className="mb-2 text-base font-bold">Hashtags</h1>
+          <p className="font-semibold text-xs text-gray-500">
+            Use hashtags to categorize your banquet and make it easily
+            discoverable.
+          </p>
+        </div>
+        <input
+          onChange={handleInputChange}
+          value={hashtags}
+          className="w-11/12 mt-2 text-black border-2 px-3 text-sm rounded-lg border-black h-10"
+        />
+        <div className="mt-5 w-11/12">
+          <h1 className="mb-2 text-base font-bold">Passcode</h1>
+          <p className="font-semibold text-xs text-gray-500">
+            Set a secure passcode to ensure that only invited guests can join
+            your private banquet.
+          </p>
+        </div>
+        <input
+          onChange={(e) => setBanquetPasscode(e.target.value)}
+          value={banquetPasscode}
+          className="w-11/12 mt-2 text-black border-2 px-3 text-sm rounded-lg border-black h-10"
+        />
+        <div onClick={createBanquet} className="w-11/12 mt-5">
+          <button className="w-full h-10 bg-black rounded-lg text-base font-bold text-white">
+            Create
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CreateBanquet;
