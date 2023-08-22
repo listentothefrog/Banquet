d472aa560a3d618072aa788cda5755f8115892df
Shashank <shashanke7y@gmail.com>
2023-08-21 21:59:53 -0400

    added navigation where user can navigate between joined banquets and banquets that they didnt join

--- src/app/discover/page.tsx
+++ src/app/discover/page.tsx
@@ -5,12 +5,13 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import BanquetCommunityCard from "@/components/BanquetCommunityCard";
 import { useEffect, useState } from "react";
 import { collection, getDocs } from "firebase/firestore";
-import HeaderComponent from "@/components/Navigation/HeaderComponet";
+import HeaderComponent from "@/components/Navigation/HeaderComponent";
 import BottomNavigation from "@/components/Navigation/BottomNavigation";
 
 const Discover = () => {
   const router = useRouter();
   const [banquet, setBanquet] = useState([]);
+  const [renderYourBanquet, setRenderYourBanquet] = useState(false);
 
   useEffect(() => {
     const getData = async () => {
@@ -33,20 +34,35 @@ const Discover = () => {
     <div className="max-w-7xl h-full">
       <HeaderComponent profilePicture={user?.photoURL || ""} />
       <main className="max-w-7xl h-full">
-        <div className="ml-3 mt-2 mb-5">
-          <p className="font-semibold text-xl">Popular Banquets 🥂</p>
+        <div className="flex items-center justify-between w-11/12 ml-3 mt-2 mb-5">
+          <p
+            onClick={() => setRenderYourBanquet(true)}
+            className="font-semibold text-base cursor-pointer"
+          >
+            Your Banquets 🥂
+          </p>
+          <p
+            onClick={() => setRenderYourBanquet(false)}
+            className="font-semibold text-base cursor-pointer"
+          >
+            Popular Banquets 🥂
+          </p>
         </div>
-        {banquet.map((data: any) => (
-          <BanquetCommunityCard
-            key={data.id}
-            title={data.title}
-            description={data.description}
-            hashtags={data.hashtags}
-            passcode={data.passcode}
-          />
-        ))}
-
-        <button onClick={() => auth.signOut()}>Sign out</button>
+        {renderYourBanquet ? (
+          <div className="">hello world</div>
+        ) : (
+          <div className="">
+            {banquet.map((data: any) => (
+              <BanquetCommunityCard
+                key={data.id}
+                title={data.title}
+                description={data.description}
+                hashtags={data.hashtags}
+                passcode={data.passcode}
+              />
+            ))}
+          </div>
+        )}
       </main>
       <BottomNavigation />
     </div>
