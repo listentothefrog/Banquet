8bda8d877aae461d15defcc43476c5f92e67758a
Shashank <shashanke7y@gmail.com>
2023-08-22 12:39:54 -0400

    create spinner component

---
+++ src/components/SpinnerComponent.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { MoonLoader } from "react-spinners";
+
+const SpinnerComponent = () => {
+  return <MoonLoader color="#000000" size={25} speedMultiplier={1} />;
+};
+
+export default SpinnerComponent;
