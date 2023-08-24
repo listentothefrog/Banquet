b106f95aa4804d523bf64b528baadd46d6c544b7
Shashank <shashanke7y@gmail.com>
2023-08-24 09:40:05 -0400

    added a popup modal when user clicks delete account and also disabled dark theme for now

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -2,11 +2,22 @@
 import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { auth } from "../../../firebase";
+import { auth, db } from "../../../firebase";
 import HeaderComponent from "@/components/Navigation/HeaderComponent";
 import SpinnerComponent from "@/components/SpinnerComponent";
-
+import Popup from "reactjs-popup";
+import "../../components/Modal.css";
+import {
+  getDocs,
+  collection,
+  where,
+  collectionGroup,
+  getDoc,
+  deleteDoc,
+  query,
+} from "firebase/firestore";
 const SettingsPage = () => {
+  const [errorMessage, setErrorMessage] = useState("");
   const [themePreference, setThemePreference] = useState(
     localStorage.getItem("themePreference") || "light"
   );
@@ -38,7 +49,7 @@ const SettingsPage = () => {
   return (
     <div
       className={`${
-        themePreference === "dark" ? "dark:bg-black text-white" : ""
+        themePreference === "dark" ? "dark:bg-black text-white" : "light-theme"
       } max-w-7xl flex flex-col h-screen`}
     >
       <HeaderComponent profilePicture={user?.photoURL || ""} />
@@ -113,7 +124,9 @@ const SettingsPage = () => {
             } px-2 flex items-center justify-between text-black h-16 border-2 border-black rounded-lg`}
           >
             <div>
-              <span className="text-lg cursor-pointer">🌙 Dark Theme</span>
+              <span className="text-lg cursor-pointer">
+                🌙 Dark Theme <span className="text-sm">(Comming Soon)</span>
+              </span>
               <p className="text-sm text-gray-500 ml-2">
                 Dive into the dark abyss! Perfect for night owls.
               </p>
@@ -122,6 +135,7 @@ const SettingsPage = () => {
               <input
                 type="radio"
                 value="dark"
+                disabled
                 checked={themePreference === "dark"}
                 onChange={() => handleThemeChange("dark")}
                 className="mr-2 cursor-pointer"
@@ -143,13 +157,49 @@ const SettingsPage = () => {
           >
             Sign Out
           </button>
-          <button
-            className={`${
-              themePreference === "dark" ? "dark:bg-white dark:text-black" : ""
-            } mt-3 font-semibold w-full px-4 py-2 bg-black text-white rounded-lg`}
+          <Popup
+            trigger={
+              <button
+                className={`${
+                  themePreference === "dark"
+                    ? "dark:bg-white dark:text-black"
+                    : ""
+                } mt-3 font-semibold w-full px-4 py-2 bg-black text-white rounded-lg`}
+              >
+                Delete Account
+              </button>
+            }
+            modal
+            nested
           >
-            Delete Account
-          </button>
+            {(close) => (
+              <div className="modal">
+                <button className="close" onClick={close}>
+                  &times;
+                </button>
+                <div className="header">Warning: Deleting Account</div>
+                <div className="content font-semibold">
+                  Are you sure you want to delete your account? This action
+                  cannot be undone.
+                  <p className="text-red-500 mt-2 font-semibold">
+                    Warning: Deleting your account will also delete all the
+                    banquets you've created and joined.
+                  </p>
+                </div>
+                <div className="actions">
+                  <button
+                    className="font-semibold w-full bg-black text-white h-10 rounded-lg"
+                    onClick={() => {
+                      close();
+                    }}
+                  >
+                    Delete Account
+                  </button>
+                  <p className="text-red-500 text-center">{errorMessage}</p>
+                </div>
+              </div>
+            )}
+          </Popup>
         </div>
       </div>
     </div>
