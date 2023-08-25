1dfd8d1cd3b940bfcbf1bd5dd97ec8adf34e704f
Shashank <shashanke7y@gmail.com>
2023-08-25 09:42:24 -0400

    maps the content in the chats subcollection and renders it

--- src/app/banquet/[name]/page.tsx
+++ src/app/banquet/[name]/page.tsx
@@ -8,23 +8,34 @@ import { db } from "../../../../firebase";
 const CommunityPage = () => {
   const pathname = usePathname();
   const modifiedPath = pathname.replace(/^\/banquet\//, "");
-
+  const [chats, setChats] = useState([]);
   useEffect(() => {
     const fetchChats = async () => {
       if (pathname) {
         const banquetDocRef = doc(db, "Banquet", modifiedPath);
         const chatsCollectionRef = collection(banquetDocRef, "chats");
         const querySnapshot = await getDocs(chatsCollectionRef);
+        const chatData: any = [];
         querySnapshot.forEach((doc) => {
-          console.log("Document ID:", doc.id);
-          console.log("Document Data:", doc.data());
+          chatData.push(doc.data());
         });
+        setChats(chatData);
       }
     };
     fetchChats();
   }, [pathname]);
 
-  return <div className="max-w-7xl">hello world</div>;
+  return (
+    <div className="max-w-7xl">
+      {chats.map((chat: any, index) => (
+        <div key={index} className="chat">
+          {/* Render chat content here */}
+          <p>{chat.message}</p>
+          <p>{chat.uid}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default CommunityPage;
