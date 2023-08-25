4fb8cc325677a04f08d80449c1524d8f959ff56a
Shashank <shashanke7y@gmail.com>
2023-08-25 11:23:43 -0400

    created chat header component

---
+++ src/components/Navigation/ChatHeader.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import React, { FC } from "react";
+interface ChatHeaderProps {
+  banquetTitle: string;
+}
+
+const ChatHeader: FC<ChatHeaderProps> = ({ banquetTitle }) => {
+  return (
+    <div className="flex items-center mt-3">
+      <div className="flex items-start justify-center ml-3">
+        <Link href="/discover">👈</Link>
+      </div>
+      <div className="w-full flex items-center justify-center">
+        <p className="text-center">{banquetTitle}</p>
+      </div>
+      <div className="flex items-start justify-center mr-3">
+        <Link href={`${banquetTitle}/settings`}>⚙️</Link>
+      </div>
+    </div>
+  );
+};
+
+export default ChatHeader;
