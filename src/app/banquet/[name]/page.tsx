31c29f91eed47ce7077a6817b8f044e4118823fe
Shashank <shashanke7y@gmail.com>
2023-08-25 11:20:35 -0400

    created a chat header screen

--- src/app/banquet/[name]/page.tsx
+++ src/app/banquet/[name]/page.tsx
@@ -2,17 +2,20 @@
 
 import { usePathname } from "next/navigation";
 import React, { useEffect, useState } from "react";
-import { collection, doc, getDocs } from "firebase/firestore";
+import { collection, doc, getDoc, getDocs } from "firebase/firestore";
 import { db } from "../../../../firebase";
+import Link from "next/link";
 
 const CommunityPage = () => {
   const pathname = usePathname();
   const modifiedPath = pathname.replace(/^\/banquet\//, "");
   const [chats, setChats] = useState([]);
+
   useEffect(() => {
     const fetchChats = async () => {
       if (pathname) {
         const banquetDocRef = doc(db, "Banquet", modifiedPath);
+
         const chatsCollectionRef = collection(banquetDocRef, "chats");
         const querySnapshot = await getDocs(chatsCollectionRef);
         const chatData: any = [];
@@ -26,14 +29,18 @@ const CommunityPage = () => {
   }, [pathname]);
 
   return (
-    <div className="max-w-7xl">
-      {chats.map((chat: any, index) => (
-        <div key={index} className="chat">
-          {/* Render chat content here */}
-          <p>{chat.message}</p>
-          <p>{chat.uid}</p>
+    <div className="max-w-7xl h-full">
+      <div className="flex items-center mt-3">
+        <div className="flex items-start justify-center ml-3">
+          <Link href="/discover">👈</Link>
         </div>
-      ))}
+        <div className="w-full flex items-center justify-center">
+          <p className="text-center">{modifiedPath}</p>
+        </div>
+        <div className="flex items-start justify-center mr-3">
+          <Link href={`${modifiedPath}/settings`}>⚙️</Link>
+        </div>
+      </div>
     </div>
   );
 };
