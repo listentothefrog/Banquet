8adf7738edef05a013d2240102ee78ca935810a7
Shashank <shashanke7y@gmail.com>
2023-08-25 14:17:14 -0400

    pulls the community title from the firestore document

--- src/app/banquet/[name]/page.tsx
+++ src/app/banquet/[name]/page.tsx
@@ -2,7 +2,7 @@
 
 import { usePathname } from "next/navigation";
 import React, { useEffect, useState } from "react";
-import { collection, doc, getDocs } from "firebase/firestore";
+import { collection, doc, getDoc, getDocs } from "firebase/firestore";
 import { db } from "../../../../firebase";
 import ChatHeader from "@/components/Navigation/ChatHeader";
 
@@ -10,8 +10,16 @@ const CommunityPage = () => {
   const pathname = usePathname();
   const modifiedPath = pathname.replace(/^\/banquet\//, "");
   const [chats, setChats] = useState([]);
+  const [banquetName, setBanquetName] = useState("");
 
   useEffect(() => {
+    const fetchBanquetName = async () => {
+      const banquetRef = doc(db, "Banquet", modifiedPath);
+      const docSnapshot = await getDoc(banquetRef);
+      if (docSnapshot.exists()) {
+        setBanquetName(docSnapshot.data().title);
+      }
+    };
     const fetchChats = async () => {
       if (pathname) {
         const banquetDocRef = doc(db, "Banquet", modifiedPath);
@@ -25,12 +33,13 @@ const CommunityPage = () => {
         setChats(chatData);
       }
     };
+    fetchBanquetName();
     fetchChats();
   }, [pathname]);
 
   return (
     <div className="max-w-7xl h-full">
-      <ChatHeader banquetTitle={modifiedPath} />
+      <ChatHeader banquetTitle={banquetName} />
     </div>
   );
 };
