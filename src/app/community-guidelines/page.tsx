ac6b26c1b69f427b12ee9e9f1a76f6461755f8b4
Shashank <shashanke7y@gmail.com>
2023-08-28 17:19:19 -0400

    created community guidelines page

---
+++ src/app/community-guidelines/page.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+
+const CommunityGuidelines = () => {
+  return (
+    <div className="m-3 rounded-md shadow-xl max-w-md w-full px-3 py-3">
+      <h1 className="text-2xl font-bold mb-4">Community Guidelines</h1>
+      <p>
+        Welcome to our community! To ensure a safe and enjoyable experience for
+        everyone, we have a few guidelines that we ask all members to follow:
+      </p>
+      <h2 className="text-base font-bold mt-4">1. Be Respectful</h2>
+      <p>
+        Treat all members with respect and kindness. Harassment, hate speech,
+        and discriminatory behavior will not be tolerated.
+      </p>
+      <h2 className="text-base font-bold mt-4">2. Keep it Friendly</h2>
+      <p>
+        Engage in conversations and discussions in a friendly and constructive
+        manner. Avoid personal attacks and aggressive behavior.
+      </p>
+      <h2 className="text-base font-bold mt-4">3. Stay on Topic</h2>
+      <p>
+        Keep your discussions relevant to the community's focus. Off-topic posts
+        and spam are discouraged.
+      </p>
+      <h2 className="text-base font-bold mt-4">4. Respect Privacy</h2>
+      <p>
+        Respect the privacy of fellow members. Do not share personal information
+        without consent.
+      </p>
+      <h2 className="text-base font-bold mt-4">5. Follow Applicable Laws</h2>
+      <p>
+        Ensure that your interactions and content comply with applicable laws
+        and regulations.
+      </p>
+      <h2 className="text-base font-bold mt-4">6. Report Violations</h2>
+      <p>
+        If you come across any content or behavior that violates these
+        guidelines, please report it to the community administrators.
+      </p>
+      <button className="w-full bg-black rounded-lg h-10 text-white font-bold mt-2">
+        Take me Back ⏲️
+      </button>
+    </div>
+  );
+};
+
+export default CommunityGuidelines;
